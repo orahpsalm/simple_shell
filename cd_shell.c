656066656301ccdc9ab2ef390a5ec4ee47b8b29c
orahpsalm <psalmorahcle@gmail.com>
2022-05-19 22:46:31 +0100

    Edit to conform to betty documentation style

--- cd_shell.c
+++ cd_shell.c
@@ -6,6 +6,7 @@
  * @datash: data relevant
  * Return: 1 on success
  */
+
 int cd_shell(data_shell *datash)
 {
 	char *dir;
@@ -19,26 +20,21 @@ int cd_shell(data_shell *datash)
 		ishome2 = _strcmp("~", dir);
 		isddash = _strcmp("--", dir);
 	}
-
 	if (dir == NULL || !ishome || !ishome2 || !isddash)
 	{
 		cd_to_home(datash);
 		return (1);
 	}
-
 	if (_strcmp("-", dir) == 0)
 	{
 		cd_previous(datash);
 		return (1);
 	}
-
 	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
 	{
 		cd_dot(datash);
 		return (1);
 	}
-
 	cd_to(datash);
-
 	return (1);
 }
